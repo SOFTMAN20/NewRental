61a3fec00836259452bbce6dfeb6501a1570ad60
milton-ux <hezronmilton1@gmail.com>
2025-09-22 14:10:14 +0300

    Changes to be committed: 	modified:   src/components/dashboard/PropertyGrid.tsx

--- src/components/dashboard/PropertyGrid.tsx
+++ src/components/dashboard/PropertyGrid.tsx
@@ -201,7 +201,7 @@ const PropertyGrid: React.FC<PropertyGridProps> = ({
       </div>
 
       {/* Properties Grid */}
-      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6">
         {properties.map(renderPropertyCard)}
       </div>
     </div>
